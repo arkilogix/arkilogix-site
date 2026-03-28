4ed0e4f357162b994a328b8af9ab39ccfbe25d41
Edxer Magpantay <edxermagpantay@gmail.com>
2026-03-28 12:04:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const params = new URLSearchParams(window.location.search);
 const client = params.get("client");
 
-if (!client) {
-  document.getElementById("title").textContent =
-    "We build smart digital systems for professionals.";
-} else {
-  fetch(`/clients/${client}.json`)
-    .then(res => res.json())
+console.log("CLIENT:", client); // debug
+
+if (client) {
+  fetch(`clients/${client}.json`)
+    .then(res => {
+      if (!res.ok) throw new Error("Not found");
+      return res.json();
+    })
     .then(data => {
       document.getElementById("name").textContent = data.name;
       document.getElementById("title").textContent = data.title;
