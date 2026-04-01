fd81f6dc41ff92b3dc5b653d1cc38b8f32a883d2
Edxer Magpantay <edxermagpantay@gmail.com>
2026-04-01 11:34:04 +0800

    Update basic.js

--- view/basic.js
+++ view/basic.js
@@ -3,6 +3,29 @@ function getClientId() {
   return params.get("id");
 }
 
-const clientId = getClientId();
+async function fetchClientData(id) {
+  try {
+    const response = await fetch(`/arkilogix-site/clients/${id}.json`);
 
-console.log("Client ID:", clientId);
+    if (!response.ok) {
+      throw new Error("File not found");
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error("ERROR:", error);
+    return null;
+  }
+}
+
+async function init() {
+  const id = getClientId();
+  console.log("Client ID:", id);
+
+  const data = await fetchClientData(id);
+  console.log("Client Data:", data);
+}
+
+init();
