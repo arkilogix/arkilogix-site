fe5f398fec7afc8527ccfcb8f6266e73c3bf066f
Edxer Magpantay <edxermagpantay@gmail.com>
2026-04-02 10:39:10 +0800

    Update app.js

--- view/app.js
+++ view/app.js
@@ -67,7 +67,7 @@ async function loadClient() {
   }
 
   try {
-    const res = await fetch(`/arkilogix-site/clients/${id}.txt`);
+    const res = await fetch(window.location.origin + "/arkilogix-site/clients/" + id + ".json");
     if (!res.ok) throw new Error("Not found");
 
     const data = await res.json();
