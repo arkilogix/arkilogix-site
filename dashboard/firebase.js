e7fb201bae981574a33060dac08ab42297143aac
Edxer Magpantay <edxermagpantay@gmail.com>
2026-04-03 13:30:50 +0800

    Update firebase.js

--- dashboard/firebase.js
+++ dashboard/firebase.js
@@ -1,13 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 🔴 PASTE YOUR REAL FIREBASE CONFIG HERE
+const firebaseConfig = {
+  apiKey: "AIzaSyCUw-qxeRg8YaihNcJPmJDHL2z6zBE6PK4",
+  authDomain: "arkilogix-clients.firebaseapp.com",
+  projectId: "arkilogix-clients",
+  storageBucket: "arkilogix-clients.firebasestorage.app",
+  messagingSenderId: "1074947351840",
+  appId: "1:1074947351840:web:b077eb79963fff59316345",
+  measurementId: "G-HFPXDVR2BZ"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firestore
 export const db = getFirestore(app);
